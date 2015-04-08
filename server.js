d49a5bdae1cdfa273a84ade63fd32840e7ef63f0
schetnikovich <tshcetnikovich@gmail.com>
2015-04-08 16:18:38 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+// The following line allows application-level modules to be required as if
+// they were installed into the node_modules directory.
+// IMPORTANT: The search path should be modified before any modules are loaded!
+require('app-module-path').addPath(__dirname + '/..');
+
+// Configure the RaptorJS Optimizer
+require('optimizer').configure({
+    plugins: [ 'optimizer-marko', 'optimizer-less' ],
+    resolveCssUrls: true,
+    projectRoot : __dirname + '/..',
+    bundlingEnabled: true,
+    outputDir: "static",  // Write all bundles into the "static" directory
+    "urlPrefix": "/static"
+});
+
+var app = require('express')();
+app.use('/static', require('serve-static')(__dirname + '/../static'));
+app.get('/', require('./src/index'));
+
+var port = 3000;
+app.listen(port, function() {
+    console.log('Listening on port %d', port);
+});
